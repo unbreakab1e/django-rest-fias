6e131ae95c046d513c0e5dc8c3154949356d4e32
Михаил Кольцов <mikekoltsov@gmail.com>
2016-04-11 14:49:32 -0400

    allow not-authenticated users

--- demo_service/settings/oauth2.py
+++ demo_service/settings/oauth2.py
@@ -12,7 +12,7 @@ REST_FRAMEWORK.update(
             'rest_framework.authentication.OAuth2Authentication',
         ),
         'DEFAULT_PERMISSION_CLASSES': (
-            'rest_framework.permissions.IsAuthenticated',
+            'rest_framework.permissions.AllowAny',
         ),
     }
 )
